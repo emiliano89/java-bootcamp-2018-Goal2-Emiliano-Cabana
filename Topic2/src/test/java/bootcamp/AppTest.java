9c4fec87d9c4b545fea09ce6cb929d93a8f8f814
emiliano89 <emic.84@gmail.com>
2018-09-25 22:27:25 +0000

    .

--- Topic2/src/test/java/bootcamp/AppTest.java
+++ Topic2/src/test/java/bootcamp/AppTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest 
-    //extends TestCase
 {
     @Test
     public void WhenRunForTheFirstTimeThenTheListIsEmpty(){
