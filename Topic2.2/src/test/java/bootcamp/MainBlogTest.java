68d3ac0532e48c488ccba04bc51166daa3793dfe
emiliano89 <emic.84@gmail.com>
2018-10-02 23:08:56 +0000

    changed variable name

--- Topic2.2/src/test/java/bootcamp/MainBlogTest.java
+++ Topic2.2/src/test/java/bootcamp/MainBlogTest.java
@@ -13,27 +13,26 @@ import static org.mockito.Mockito.when;
 public class MainBlogTest {
     String entry = null;
       
-   Blog Mockblog = mock(Blog.class);
+   Blog mockblog = mock(Blog.class);
    MainBlog mainblog = new MainBlog(Mockblog);
    
    @Test
    public void testPostEntry(){
-//      entry = "Hello world";
-      when(Mockblog.postEntry("Hello world")).thenReturn("Hello world");
+      when(mockblog.postEntry("Hello world")).thenReturn("Hello world");
       assertEquals("Hello world", mainblog.postEntry("Hello world"));
    }
    
    @Test
    public void testDeleteEntry() { 
       int i = 0;
-      when(Mockblog.deleteEntry(i)).thenReturn(null);
+      when(mockblog.deleteEntry(i)).thenReturn(null);
       assertEquals(null, mainblog.deleteEntry(i));
    }
       
    @Test
    public void testMostRecentEntries() {
     List mockList = new ArrayList();
-    when(Mockblog.recentEntries(entry)).thenCallRealMethod();
+    when(mockblog.recentEntries(entry)).thenCallRealMethod();
     mockList.add(mainblog.recentEntries(entry = "a"));
     mockList.add(mainblog.recentEntries(entry = "b"));
     mockList.add(mainblog.recentEntries(entry = "c"));
@@ -46,7 +45,7 @@ public class MainBlogTest {
     mockList.add(mainblog.recentEntries(entry = "j"));
     mockList.add(mainblog.recentEntries(entry = "k"));
     mockList.add(mainblog.recentEntries(entry = "l"));
-    when(Mockblog.recentEntryList()).thenReturn(mockList);
+    when(mockblog.recentEntryList()).thenReturn(mockList);
     assertEquals("[k, l, c, d, e, f, g, h, i, j]", mainblog.recentEntryList().toString());
    }
 }
